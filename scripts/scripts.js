d9ad68194cf5099cde5db93808fac8ec704b1606
Ishan Jain <ishan@jain.se>
2015-08-16 19:08:51 +0200

    Some fixes for the menu.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 (function($, w, f, u) {
   $(document).ready(function() {
-    $(".nav-icon").click(function() {
+    $(".nav-icon-wrapper .nav-icon.menu").click(function() {
       $(".main-nav").toggleClass("show");
     });
   });
