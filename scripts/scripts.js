931b424a3862ac3db5450291580e3f016c597b6e
Ishan Jain <ishan@jain.se>
2015-08-17 08:54:40 +0200

    Header is turned transparent on scroll.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,5 +3,12 @@
     $(".nav-icon-wrapper .nav-icon.menu").click(function() {
       $(".main-nav").toggleClass("show");
     });
+    $(".main-wrapper").scroll(function () {
+      var $top = $(".top-header", this);
+      if ($(this).scrollTop() != 0)
+        $top.addClass("glued");
+      else
+        $top.removeClass("glued");
+    });
   });
 })(jQuery, window, false);
